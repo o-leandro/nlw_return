0067a9a344e2800ffbd0cef9f6e5c7269d7698cc
Leandro Santos <leanddrosanttoss@gmail.com>
2022-05-07 12:27:55 -0300

    Ajustes em larguras de telas de 320 a 500px e de 1024px para cima. Reestruturação no html. Ajustes no menu.

--- main.js
+++ main.js
@@ -5,7 +5,59 @@ onScroll();
 function onScroll() {
   showNavOnScroll();
   showBackToTopButtonOnScroll();
+
+  activateMenuAtCurrentSection(home);
+  activateMenuAtCurrentSection(services);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
+  
 }
+
+function activateMenuAtCurrentSection(section) {
+  // Linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // Verificar se a seção passou da linha
+  // Quais dados vou precisar?
+
+  // O topo da seção
+  const sectionTop = section.offsetTop
+
+  // A altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // O topo da seção chegou ou ultrapassou a linha alto
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop;
+
+  console.log('O topo da seção chegou ou passou da linha? ' + sectionTopReachOrPassedTargetLine);
+
+  // Verificar se a base está abaixo da linha alvo
+  // Quais dados vou precisar?
+
+  // A seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // O final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine;
+
+  console.log('O fundo da seção passou da linha? ', sectionEndPassedTargetLine);
+
+  // Limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document
+  .querySelector(`.menu a[href*=${sectionId}]`)
+
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+
+
 
 function showNavOnScroll() {
   if (scrollY > 0) {
